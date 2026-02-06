cd18f74257582733c248f70ae83d153ee5dbcbd5
a67712 <a67712@alunos.ipb.pt>
2026-02-06 19:18:53 +0000

    Version 1 - base component

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -3,18 +3,30 @@ import React, { Component } from 'react'
 class Dice extends Component {
     constructor(props) {
         super(props);
+        this.state={diceNumber:1}
     }
+    tossDice = () =>{let newNum =  Math.floor(Math.random()*6)+1;
+        this.setState({diceNumber:newNum})
+
+    };
+    
 
     render() {
-        return (<div>
-            <h1>Icon: </h1>
-            <i className="fas fa-dice-one" style={{ fontSize: '50px', color: 'purple' }}> </i>
-            <i className="fa-solid fa-dice-two" style={{ fontSize: '50px', color: 'purple' }}></i>
-            <i className="fa-solid fa-dice-three" style={{ fontSize: '50px', color: 'purple' }}></i>
-            <i className="fa-solid fa-dice-four" style={{ fontSize: '50px', color: 'purple' }}></i>
-            <i className="fa-solid fa-dice-five" style={{ fontSize: '50px', color: 'purple' }}></i>
-            <i className="fa-solid fa-dice-six" style={{ fontSize: '50px', color: 'purple' }}></i>
 
+        const faces = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+         const faceText  = faces[this.state.diceNumber -1];
+
+
+        const iconClass = `fas fa-dice-${faceText}`;
+
+       
+        return (<div>
+      <h1>Roll the dice!</h1>
+      <i className={iconClass} style={{fontSize: '100px',color: 'purple'}}>
+</i>
+<br/><br/>
+<button onClick={this.tossDice} style={{padding: '20px'}}>Toss Dice</button>
 
         </div>)
     }
